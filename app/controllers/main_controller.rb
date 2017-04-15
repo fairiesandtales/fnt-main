a7b4505fd8e89be7496c457f865816163f5236d7
Mikola Peter <mikopet@gmail.com>
2017-04-15 20:44:39 +0200

    a bit of cleanup adn some frontend stuff

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,3 +1,7 @@
 class MainController < ApplicationController
   def index; end
+  def about; end
+  def wiki
+    # Item.all, Monster.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'main#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/about', to: 'main#about', as: 'about'
+  get '/wiki', to: 'main#wiki', as: 'wiki'
 end
