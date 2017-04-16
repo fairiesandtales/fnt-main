8d0bc2da2cd385de3f558a25ce1cb1d04f1380a4
Mikola Peter <mikopet@gmail.com>
2017-04-16 16:27:06 +0200

    some database fun to make devise work correctly

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -252,8 +252,8 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   config.omniauth :google,
-                  ENV.fetch('GOOGLE_OAUTH_ID'),
-                  ENV.fetch('GOOGLE_OAUTH_SECRET')
+                  ENV.fetch('GOOGLE_OAUTH_ID') { '' },
+                  ENV.fetch('GOOGLE_OAUTH_SECRET') { '' }
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
